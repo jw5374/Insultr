ec0758b632c163fa80be88b56a4c506155e5e05f
jw5374 <jw5374@nyu.edu>
2022-01-17 16:16:42 -0500

    improved smaller screen sizes, word cycle effect

--- public/scripts/fetchData.js
+++ public/scripts/fetchData.js
@@ -3,12 +3,34 @@ const generateButton = document.querySelector("#insultspin")
 const advButton = document.querySelector("#adverbspin")
 const adjButton = document.querySelector("#adjectivespin")
 const nounButton = document.querySelector("#nounspin")
+const cyclesetting = document.querySelector("#togglecycle")
 const url = "http://localhost:8070"
+let cycleIndicator = document.querySelector("#cycleindicator")
 let adverbs, adjectives, nouns
-let advIndex, adjIndex, nounIndex
+let indexes =  {
+    advIndex: null,
+    adjIndex: null,
+    nounIndex: null
+}
+
+function timer(ms) {
+    return new Promise(res => setTimeout(res, ms))
+}
 
 function randomIndex(max) {
     return Math.floor(Math.random() * max)
+}
+
+function setLine() {
+    insultLine.textContent = `${adverbs.data[indexes.advIndex]} ${adjectives.data[indexes.adjIndex]} ${nouns.data[indexes.nounIndex]}!`
+}
+
+async function cycleWords(index, value, key) {
+    for(let i = value-7; i <= value; i++) {
+        index[key] = i
+        await timer((1/(value-i)) * 300)
+        setLine()
+    }
 }
 
 async function fetchAll() {
@@ -35,24 +57,46 @@ async function fetchAll() {
 
 fetchAll()
 
+cyclesetting.addEventListener("click", (e) => {
+    cycleIndicator.textContent = cycleIndicator.textContent == "On" ? "Off" : "On"
+})
+
 generateButton.addEventListener("click", (e) => {
-    advIndex = randomIndex(adverbs.data.length)
-    adjIndex = randomIndex(adjectives.data.length)
-    nounIndex = randomIndex(nouns.data.length)
-    insultLine.textContent = `${adverbs.data[advIndex]} ${adjectives.data[adjIndex]} ${nouns.data[nounIndex]}!`
+    indexes.advIndex = randomIndex(adverbs.data.length)
+    indexes.adjIndex = randomIndex(adjectives.data.length)
+    indexes.nounIndex = randomIndex(nouns.data.length)
+    if(cycleIndicator.textContent == "On") {
+        cycleWords(indexes, indexes.advIndex, "advIndex")
+        cycleWords(indexes, indexes.adjIndex, "adjIndex")
+        cycleWords(indexes, indexes.nounIndex, "nounIndex")
+    } else {
+        setLine()
+    }
 })
 
 advButton.addEventListener("click", (e) => {
-    advIndex = randomIndex(adverbs.data.length)
-    insultLine.textContent = `${adverbs.data[advIndex]} ${adjectives.data[adjIndex]} ${nouns.data[nounIndex]}!`
+    indexes.advIndex = randomIndex(adverbs.data.length)
+    if(cycleIndicator.textContent == "On") {
+        cycleWords(indexes, indexes.advIndex, "advIndex")
+    } else {
+        setLine()        
+    }
 })
 
 adjButton.addEventListener("click", (e) => {
-    adjIndex = randomIndex(adjectives.data.length)
-    insultLine.textContent = `${adverbs.data[advIndex]} ${adjectives.data[adjIndex]} ${nouns.data[nounIndex]}!`
+    indexes.adjIndex = randomIndex(adjectives.data.length)
+    if(cycleIndicator.textContent == "On") {
+        cycleWords(indexes, indexes.adjIndex, "adjIndex")
+    } else {
+        setLine()
+    }
 })
 
 nounButton.addEventListener("click", (e) => {
-    nounIndex = randomIndex(nouns.data.length)
-    insultLine.textContent = `${adverbs.data[advIndex]} ${adjectives.data[adjIndex]} ${nouns.data[nounIndex]}!`
+    indexes.nounIndex = randomIndex(nouns.data.length)
+    if(cycleIndicator.textContent == "On") {
+        cycleWords(indexes, indexes.nounIndex, "nounIndex")
+    } else {
+        setLine()
+    }
 })
